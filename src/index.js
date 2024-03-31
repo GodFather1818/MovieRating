f21b294f1bd350dd043a72b0c7922b6ce2688b6b
GodFather1818 <sahilshah0620@gmail.com>
2024-04-01 00:13:32 +0530

    Fixing the eslint error

--- src/index.js
+++ src/index.js
@@ -2,16 +2,8 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
 import App from "./App_V2";
-import StarRating from "./StarRating";
 
-function Test() {
 
-  const [movieRating, setMovieRating] = useState(0);
-  return <div>
-    <StarRating color="gold" maxRating={10} onSetRating={setMovieRating} />
-    <p>This movie was rated {movieRating} Stars. </p>
-  </div>
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
